2ba900856e7f7d0497445524daded3c8091b2a47
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-16 16:08:08 +0300

    feat: add TwoDimensionalArraySorter task 4 solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,42 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+
+    BOOL isOneDimensionalArray = true;
+    for (NSArray* item in array) {
+        if ([item isKindOfClass:[NSArray class]]) {
+            isOneDimensionalArray = false;
+        }
+    }
+    if (isOneDimensionalArray) {
+        return @[];
+    }
+    NSMutableArray *digitsArray = [[NSMutableArray alloc] init];
+    NSMutableArray *stringsArray = [[NSMutableArray alloc] init];
+    
+    for (NSArray* item in array) {
+        
+        for (int i = 0; i < item.count; i++) {
+            if ([[item objectAtIndex:i] isKindOfClass:[NSString class]]) {
+                [stringsArray addObject: [item objectAtIndex:i]];
+            }
+            if ([[item objectAtIndex:i] isKindOfClass:[NSNumber class]]) {
+                [digitsArray addObject: [item objectAtIndex:i]];
+            }
+        }
+    }
+    
+    NSArray *sortedDigitsArray = [digitsArray sortedArrayUsingSelector: @selector(compare:)];
+    NSArray *sortedStringsArray = [stringsArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    if ((sortedDigitsArray.count != 0) && (sortedStringsArray.count == 0)) {
+        return sortedDigitsArray;
+    } else if ((sortedDigitsArray.count == 0) && (sortedStringsArray.count != 0)) {
+        return sortedStringsArray;
+    } else {
+        return @[sortedDigitsArray, sortedStringsArray];
+    }
 }
 
 @end
+
