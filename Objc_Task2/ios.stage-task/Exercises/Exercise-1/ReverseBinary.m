58d0f74a3a150ef5ef0696aa3d7765c637aa961a
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-16 15:57:11 +0300

    Feat: add ReversBinary task solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,29 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSString *string = @"";
+    NSUInteger x = n;
+
+    while(x>0) {
+        string = [[NSString stringWithFormat: @"%lu", x&1] stringByAppendingString:string];
+        x = x >> 1;
+    }
+    if (string.length < 8) {
+        NSMutableString *bitString = [[NSMutableString alloc] initWithCapacity:8];
+        int zeroCount = abs(string.length - 8);
+        for (int i = 1; i <= zeroCount; i++) {
+            [bitString appendString:@"0"];
+        }
+        [bitString appendString: string];
+        string = [NSString stringWithString:bitString];
+    }
+    
+    int len = [string length];
+    NSMutableString *reverseName = [[NSMutableString alloc] initWithCapacity:len];
+    for(int i=len-1; i>=0; i--){
+        [reverseName appendFormat:[NSString stringWithFormat:@"%c",[string characterAtIndex:i]]];
+    }
+    long result = strtol([reverseName UTF8String], NULL, 2);
+    return result;
 }
+
