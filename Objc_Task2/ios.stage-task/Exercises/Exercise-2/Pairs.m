b4a6a82a712862d88c7cc6b770fd501b01500e6e
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-16 16:01:36 +0300

    feat: add Paris task2 solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,17 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int count = 0;
+
+    for (NSNumber* item in array) {
+        for (NSNumber* item1 in array) {
+            if ((item.intValue - item1.intValue) == number.intValue) {
+                count += 1;
+            }
+        }
+    }
+    return count;
 }
 
 @end
+
